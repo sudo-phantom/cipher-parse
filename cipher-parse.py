c117061b2e25e6fd3104a97e580798a034878823
sudo-phantom <sudophantom@gmail.com>
2021-09-21 16:58:22 -0700

    Update cipher-parse.py

--- cipher-parse.py
+++ cipher-parse.py
@@ -7,8 +7,7 @@ import subprocess
 
 
 BASE_URL = "https://crt.sh/?q={}&output=json"
-
-   
+  
 parser = argparse.ArgumentParser(epilog='\tExample: \r\npython3 ' + sys.argv[0] + " -d google.com")
 parser._optionals.title = "OPTIONS"
 parser.add_argument('-d', '--domain', type=str, help='Specify Target Domain to get subdomains from crt.sh', required=True)
@@ -17,33 +16,44 @@ foo = query.domain
 bar = 'common_name'
 
 # get json output from crt.sh and output to file named output.json
-r = requests.get(BASE_URL.format(str(foo)), timeout=25)
-snap = r.json()
-most = json.dumps(snap)
-with open('output.json', 'w') as file:
-    file.write(most)
+def get_out():
+    r = requests.get(BASE_URL.format(str(foo)), timeout=25)
+    snap = r.json()
+    most = json.dumps(snap)
+    with open('output.json', 'w') as file:
+        file.write(most)
+
 
 # read output.json, and search for value of bar
-
-f = open('output.json',)
-data = json.load(f)
-query_search = []
-for i in data:
-    if bar in i:
-        query_search.append(i[bar])
-    else:
-        print("No common name found for ", foo)
-f.close()
-
-print(query_search)
+def read_out():
+    f = open('output.json',)
+    data = json.load(f)
+    query_search = []
+    for i in data:
+        if bar in i:
+            query_search.append(i[bar])            
+        else:
+            print("No common name found for ", foo)
+    f.close()
+    return query_search
+      
+    
 
 # make hostfile to runn sslyze aginst
-hostlist = open('hostlist.txt', 'w')
-for element in query_search:
-    hostlist.write(element + '\n')
-hostlist.close()
+def get_host(query):
+    hostlist = open('hostlist.txt', 'w')
+    for element in query:
+        hostlist.write(element + '\n')
+    hostlist.close()
 
 #run SSLyze
-subprocess.run(['sslyze', '--regular', '--json_out=scan-results.json', '--targets_in=hostlist.txt' ])
+def get_scan():
+    subprocess.run(['sslyze', '--regular', '--json_out=scan-results.json', '--targets_in=hostlist.txt' ])
+
+get_out()
+query=read_out()
+get_host(query)
+get_scan()
+
 
 
