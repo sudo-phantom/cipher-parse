5c4b3e774cc8bbfc1ba2f41822a97e34fffd3335
sudo-phantom <sudophantom@gmail.com>
2021-09-24 16:29:34 -0700

    Update cipher-parse.py

--- cipher-parse.py
+++ cipher-parse.py
@@ -4,7 +4,6 @@ import argparse
 import sys
 import json
 import subprocess
-import os
 
 
 BASE_URL = "https://crt.sh/?q={}&output=json"
@@ -46,43 +45,3 @@ hostlist.close()
 
 #run SSLyze
 subprocess.run(['sslyze', '--regular', '--json_out=scan-results.json', '--targets_in=hostlist.txt' ])
-
-#parse results for compliance
-
-def scan_results():
-    BAD_CIPHERS = ['PSK_AES128_CBC_SHA', 'PSK_AES256_CBC_SHA' , 'PSK_AES128_GCM_SHA256', 'PSK_AES256_GCM_SHA384', 'ECDHE_PSK_AES128', 'ECDHE_PSK_AES256', 'RC4', 'NULL', 'IDEA', 'DES', '3DES', 'EDH', 'ADH', 'DH', 'DHE', 'CAMELLIA', 'SEED', 'ECDH', 'AECDH', 'EXP1024']
-    try:
-        os.makedirs("./out")
-    except FileExistsError:
-    # directory already exists
-        pass
-
-    with open('scan-results.json', 'r') as r:
-        data = json.load(r)
-    ssl2 = json.dumps(data['server_scan_results'][0]['scan_commands_results']['ssl_2_0_cipher_suites']['accepted_cipher_suites'])
-    ssl3 = json.dumps(data['server_scan_results'][0]['scan_commands_results']['ssl_3_0_cipher_suites']['accepted_cipher_suites'])
-    tls10 = json.dumps(data['server_scan_results'][0]['scan_commands_results']['tls_1_0_cipher_suites']['accepted_cipher_suites'])
-    tls11 = json.dumps(data['server_scan_results'][0]['scan_commands_results']['tls_1_1_cipher_suites']['accepted_cipher_suites'])
-    tls12 = json.dumps(data['server_scan_results'][0]['scan_commands_results']['tls_1_2_cipher_suites']['accepted_cipher_suites'])
-    tls13 = json.dumps(data['server_scan_results'][0]['scan_commands_results']['tls_1_2_cipher_suites']['accepted_cipher_suites'])
-    print('----------------BAD CIPHER LIST ------\n')
-    if ssl2 != '[]':
-        print('Illegal usage of SSLv2')
-    if ssl3 != '[]':
-        print('Illegal usage of SSLv3')
-    if tls10 != '[]':
-        print('Illegal usage of TLSv1.0')
-    with open('./out/tls1-1.json', 'w') as file:
-        file.write(tls11)
-    with open('./out/tls1-2.json', 'w') as file:
-        file.write(tls12)
-    with open('./out/tls1-3.json', 'w') as file:
-        file.write(tls13)
-    for item in BAD_CIPHERS:
-        if item in tls11:
-            print(item + ' Found in TLSv1.1')
-        elif item in tls12:
-            print(item + ' Found in TLSv1.2')
-        elif item in tls13:
-            print(item + ' Found in TLSv1.3')
-scan_results()
